84357fea9ab3f315f07448aa51b0afcab0cef7dd
Vlad Bolkunov <gloryson@mail.ru>
2022-12-14 19:42:22 +0100

    feat: added redirect if device doesn't match requirements

---
+++ js/checkDevice.js
@@ -0,0 +1,3 @@
+if (window.innerWidth < 1200 || window.innerWidth < window.innerHeight || window.matchMedia("(any-hover: none)").matches) {
+  window.location.href = 'https://gloryson.github.io/gloryson';
+}
